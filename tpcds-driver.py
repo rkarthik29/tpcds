21ff42aeac538278de5ba2bf81c2c676fadb7f9e
Karthik Narayanan <rkarthik29@users.noreply.github.com>
2025-04-01 10:11:29 -0400

    Update tpcds-driver.py

--- tpcds-driver.py
+++ tpcds-driver.py
@@ -16,14 +16,16 @@ class TpcdsRunner():
             tables = os.listdir(parsed.path)
         elif parsed.scheme == 's3a':
             fs= S3FileSystem()
+            print(parsed.path)
             tables = fs.ls(parsed.path)
         else:
             raise Exception(f'Unsupported scheme: {parsed.scheme}')
-        for table in os.listdir(self.location):
-            if table=='.DS_Store' or table.strip() == '':
+        for table in tables[1:]:
+            if table=='.DS_Store' or table.strip() == '' or table==parsed.path:
                 continue
-            df = self.spark.read.parquet(f'{self.location}/' + table)
-            df.createOrReplaceTempView(table)
+            print(table)
+            df = self.spark.read.parquet(f"s3a://{table}")
+            df.createOrReplaceTempView(table.split('/')[-1])
 
     def run_query(self, queries=[]):
         if not queries:
@@ -37,20 +39,21 @@ class TpcdsRunner():
             runtimes[query] = time.time() - start
         return runtimes
 
-os.environ['HADOOP_CONF_DIR'] = '/etc/hadoop/conf'  
-os.environ['YARN_CONF_DIR'] = '/etc/hadoop/conf'  
-
-conf=SparkConf() \
-    .setMaster("local[*]") \
-    .set("spark.executor.memory", "4g")\
-    .set("spark.driver.memory", "4g") \
-    .set("spark.driver.extraClassPath", "/usr/lib/hadoop/*") \
-    .set("spark.executor.extraClassPath", "/usr/lib/hadoop/*") 
-
-sc = SparkContext(conf=conf)
-spark = SparkSession(sc)
-data_location = os.getenv('TPCDS_DATA_LOCATION', 'data/')
-runner=TpcdsRunner(spark, data_location)
-runner.load_tables()
-runtimes = runner.run_query(queries=['q23a', 'q23b', 'q14a', 'q14b'])
-print(runtimes)
+if __name__="__main__":
+    os.environ['HADOOP_CONF_DIR'] = '/etc/hadoop/conf'  
+    os.environ['YARN_CONF_DIR'] = '/etc/hadoop/conf'
+    classpath="/var/lib/hadoop-hdfs/tpcds/gluten-velox-bundle-spark3.5_2.12-amzn_2023_x86_64-1.4.0-SNAPSHOT-nolic.jar:/opt/tpcds/jars/jackson-databind-2.15.2.jar:/opt/tpcds/jars/jackson-annotations-2.15.2.jar:/usr/lib/hadoop/hadoop-common.jar:/usr/lib/hadoop/hadoop-hdfs.jar:/usr/lib/hadoop/hadoop-aws.jar:/usr/share/aws/aws-java-sdk/aws-java-sdk-bundle-1.12.705.jar:/usr/share/aws/aws-java-sdk-v2/aws-sdk-java-bundle-2.23.18.jar:/usr/lib/hadoop/lib/*"
+    conf=SparkConf() \
+        .setMaster("local[*]") \
+        .set("spark.executor.memory", "4g")\
+        .set("spark.driver.memory", "4g") \
+        .set("spark.driver.extraClassPath", classpath) \
+        .set("spark.executor.extraClassPath",classpath) 
+    
+    sc = SparkContext(conf=conf)
+    spark = SparkSession(sc)
+    data_location = os.getenv('TPCDS_DATA_LOCATION', 'data/')
+    runner=TpcdsRunner(spark, data_location)
+    runner.load_tables()
+    runtimes = runner.run_query(queries=['q23a', 'q23b', 'q14a', 'q14b'])
+    print(runtimes)
